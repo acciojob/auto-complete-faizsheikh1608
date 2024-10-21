154e7f29f5f9576cb6faa38b46cff169f148a253
Faiz <sheikhfaiz7861@gmail.com>
2024-10-21 20:54:42 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
+  const [data, setData] = useState(fruits);
+  const [updateArray, setUpdateArray] = useState(fruits);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Search Item</h1>
+      <input
+        type="search"
+        onChange={(e) => {
+          const filterdata = data.filter((curr) =>
+            curr.includes(e.target.value)
+          );
+          setUpdateArray(filterdata);
+        }}
+      ></input>
+      <ul>
+        {updateArray.map((curr, idx) => (
+          <li key={idx}>{curr}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
